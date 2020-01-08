bf6d61e95c1ab53ab86dbc18cf65fc0891bd096c
Habi Sandurov <habi.alonso98@gmail.com>
2020-01-08 23:35:54 +0600

    added in connect mongoose links

--- api/server.js
+++ api/server.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const links = require('./app/links');
 
 
 const app = express();
-
 app.use(express.json);
 
 const port = 8000;
 
 mongoose.connect('mongodb://localhost/links', {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true}).then(() => {
-
+    app.use('/links', links);
     app.listen(port, () => {
         console.log(`Server started on ${port} port`)
     })
